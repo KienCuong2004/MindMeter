9dbc2196438fc890ca491444a3696c6b9ddd8cef
KienCuong2004 <kiencuongdev1204@gmail.com>
2025-11-15 17:46:25 +0700

    Fix blog API endpoints: Add /api prefix to baseURL in blogService

--- frontend/src/services/blogService.js
+++ frontend/src/services/blogService.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:8080/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
 class BlogService {
   constructor() {
     this.api = axios.create({
-      baseURL: `${API_BASE_URL}/blog`,
+      baseURL: `${API_BASE_URL}/api/blog`,
       headers: {
         "Content-Type": "application/json",
       },
